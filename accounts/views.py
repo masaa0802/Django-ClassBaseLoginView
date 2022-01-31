d387ab9cac7691cf53ad4028e33d609f2eb33b32
Your Name <masaakigoto0802@gmail.com>
2022-01-31 22:28:37 +0900

    ECサイトサンプル作成開始

--- accounts/views.py
+++ accounts/views.py
@@ -10,29 +10,12 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
-
 class HomeView(TemplateView):
   template_name = 'home.html'
 
 class RegistUserView(CreateView):
   template_name = 'regist.html'
   form_class = RegistForm
-
-# class UserLoginView(FormView):
-#   template_name = 'user_login.html'
-#   form_class = UserLoginForm
-
-#   def post(self, request, *args, **kwargs):
-#     email = request.POST['email']
-#     password = request.POST['password']
-#     user = authenticate(email=email, password=password)
-#     next_url = request.POST['next']
-#     if user is not None and user.is_active:
-#       login(request, user)
-#     if next_url:
-#       return redirect(next_url) 
-#     return redirect('accounts:home')
 
 class UserLoginView(LoginView):
   template_name = 'user_login.html'
@@ -44,20 +27,12 @@ class UserLoginView(LoginView):
       self.request.session.set_expiry(1200000)
     return super().form_valid(form)
 
-
-# class UserLogoutView(View):
-#   def get(self, request, *args, **kwargs):
-#     logout(request)
-#     return redirect('accounts:user_login')
-
 class UserLogoutView(LogoutView):
   pass
 
-# @method_decorator(login_required, name='dispatch')
 class UserView(LoginRequiredMixin,TemplateView):
   template_name = 'user.html'
 
-  # @method_decorator(login_required)
   def dispatch(self,*args,**kwargs):
     return super().dispatch(*args, **kwargs)
 
